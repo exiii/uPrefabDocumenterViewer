3fc93e74f9c3693ddf7320d12438c1e6cf1a38a6
tata <pf35301@gmail.com>
2019-01-24 14:30:27 +0900

    [add] Viewer Window Label Class

---
+++ Assets/PrefabDocumenterViewer/Editor/Scripts/Viewer/ConstantClass/ViewerWindowLabel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrefabDocumenter.Unity
+{
+    public class ViewerWindowLabel
+    {
+        public const string MENU_ITEM_ATTR = "Window/PrefabDocumenter/Viewer";
+        public const string NAME_SEARCH_BOX = "FileName";
+        public const string GUID_SEARCH_BOX = "Guid";
+
+    }
+}
+
